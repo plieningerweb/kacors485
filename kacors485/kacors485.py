ce932aab8859db9a03d579869cb479bf5657fada
Andreas Plieninger <plieningerweb@gmail.com>
2016-01-26 12:37:26 +0100

    Fix: Answers are sometimes not clear, still work with it Fix: In some messages no cmd type is returned Fix: Add answer information to exceptions

--- kacors485/kacors485.py
+++ kacors485/kacors485.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import serial
+
 
 class KacoRS485Parser(object):
     """
@@ -7,30 +9,57 @@ class KacoRS485Parser(object):
 
     mapping = {}
     mapping[0] = {
+#            -1: {'name': 'garbage'},
             0: {'name' : 'last_command_sent',
                 'description': 'Adresse & Fernsteuerbefehl'},
             1: {'name': 'status',
-                'description': 'Status des Wechselrichters'},
+                'description': """Status des Wechselrichters\n
+Nicht sicher, aber könnte etwa so sein:
+0 Wechselrichter hat sich gerade eingeschaltet Nur nach erstem Einschalten am Morgen
+1 Wartemodus. Netz- und Solarspannung testen: Wechselrichter wartet bis die Spannungen
+sicher anstehen und mit der Einspeisung begonnen werden kann
+2 Warten auf Ausschalten Generatorspannung und -leistung ist zu gering.
+Zustand bevor in die Nachtabschaltung übergegangen wird
+3 Konstantspannungsregler Beim Einspeisebeginn wird kurzzeitig mit
+konstanter Generatorspannung eingespeist (80% der gemessenen Leerlaufspannung)
+4 MPP- Regler, ständige Suchbewegung Bei geringer Einstrahlung wird mit suchendem
+MPP- Regler eingespeist
+5 MPP- Regler, ohne Suchbewegung Bei hoher Einstrahlung wird für maximalen
+Ertrag mit patentiertem MPP- Regler eingespeist
+9 Fehlersuchbetrieb Bei Auftreten eines internen Fehlers
+10 Übertemperaturabschaltung Bei Überhitzung des Wechselrichters
+(Kühlkörpertemperatur >80°C) durch ständige Überlastung und fehlende Luftzirkulation,
+schaltet sich der Wechselrichter ab. Ursache: Zu großer Solargenerator, zu hohe
+Umgebungstemperatur, Wechselrichterdefekt.
+11 Leistungsbegrenzung Schutzfunktion des Wechselrichters, wenn zu viel Generatorleistung geliefert wird oder der
+Kühlkörper des Gerätes heißer als 65°C wurde
+12 Überlastabschaltung Schutzfunktion des Wechselrichters, wenn zu
+viel Generatorleistung geliefert wird
+13 Überspannungsabschaltung Schutzfunktion des Wechselrichters, wenn Netzspannung zu hoch ist
+14 Netzausfall (3-phasige Überwachung) Schutzfunktion des Wechselrichters, wenn eine
+der drei Netzphasen ausgefallen ist oder die Spannung außerhalb der Toleranz ist.
+15 Übergang zur Nachtabschaltung Wechselrichter legt sich schlafen
+"""               },
             2: {'name': 'u_dc',
-                'description': 'Generator Spannung'},
+                'description': 'Generator Spannung in V *10'},
             3: {'name': 'i_dc',
-                'description': 'Generator Strom'},
+                'description': 'Generator Strom in A 100'},
             4: {'name': 'p_dc',
-                'description': 'Generator Leistung'},
+                'description': 'Generator Leistung in W'},
             5: {'name': 'u_ac',
-                'description': 'Netz Spannung'},
+                'description': 'Netz Spannung in V *10'},
             6: {'name': 'i_ac',
-                'description': 'Netz Strom'},
+                'description': 'Netz Strom in A *100'},
             7: {'name': 'p_ac',
-                'description': 'Einspeiseleistung'},
+                'description': 'Einspeiseleistung in W'},
             8: {'name':  'temp',
-                'description': 'Geraetetemperatur'},
+                'description': 'Geraetetemperatur in °C'},
             9: {'name': 'e_day',
-                'description': 'Tagesenergie'},
-            10: {'name': 'type',
-                'description': 'Wechselrichter Typ'},
-            11: {'name': 'checksum',
+                'description': 'Tagesenergie in Wh'},
+            10: {'name': 'checksum',
                 'description': 'Pruefsumme'},
+            11: {'name': 'type',
+                'description': 'Wechselrichter Typ'},
         }
     mapping[3] = {
             0: {'name' : 'last_command_sent',
@@ -40,36 +69,37 @@ class KacoRS485Parser(object):
             2: {'name': 'e_day',
                 'description': 'Tagesenergie in Wh'},
             3: {'name': 'no_idea',
-                'description': 'Zaehler der Hochzaehlt, ka was'},
+                'description': 'Zaehler der Hochzaehlt, ka was, evlt auch Gesamtenergie in kWh'},
             4: {'name': 'e_all',
-                'description': 'Gesamtenergie in 0.1kWH'},
-            5: {'name': 'counter_h',
-                'description': 'Zaehler Stunden'},
-            6: {'name': 'run_today',
-                'description': 'Betriebsstunden heute in h'},
-            7: {'name': 'run_all',
-                'description': 'Betriebsstunden gesamt in h'},
-            8: {'name': 'checksum',
-                'description': 'Pruefsumme'},
+                'description': 'Gesamtenergie in kWh'},
+            5: {'name': 'run_today',
+                'description': 'Betriebsstunden heute in hh:mm'},
+            6: {'name': 'run_all',
+                'description': 'Betriebsstunden gesamt in h:mm'},
+            7: {'name': 'run_all_again',
+                'description': 'Betriebsstunden gesamt, evtl Zaehler in h:mm'},
+#            8: {'name': 'checksum',
+#                'description': 'Pruefsumme'},
         }
 
 
-    def parse(self,answer):
+    def parse(self,answer,command):
         #split on any whitespace
+        answer = answer.replace('\r','').replace('\x00','').replace('\n','')
         l = answer.split()
 
         #parse first field
-        command = l[0]
-        inverterName = command[1:3]
-        sentCommand = int(command[-1])
+        sentCommand = int(command[-3])
 
         if not sentCommand in self.mapping:
-            raise Exception('unknown command "{}" to parse'.format(sentCommand))
+            raise Exception('unknown command "{}" to parse in {:s}'.format(sentCommand, repr(answer)))
 
         mymap = self.mapping[sentCommand]
 
         if len(l) != len(mymap):
-            raise Exception('length of answer and template not the same')
+            print("input",l,"len",len(l))
+            print("map",mymap,"len",len(mymap))
+            raise Exception('length of answer and template not the same: {:s}'.format(repr(answer)))
 
 
         ret = {}
@@ -103,7 +133,7 @@ class KacoRS485(object):
         * reading current values
     """
 
-    waitBeforeRead = 0.3
+    waitBeforeRead = 0.7
 
     def __init__(self,serialPort):
         """
@@ -138,17 +168,15 @@ class KacoRS485(object):
         inverterNumber: can be between 0 and 32
         """
 
-        answers = []
+        answers = {}
 
-        newline = '\r\n'
-
-        sendCommands = [0,2,3]
+        sendCommands = [0,3]
         commands = []
         for s in sendCommands:
             commands.append('#{:02d}{:01d}\r\n'.format(inverterNumber,s))
 
         for cmd in commands:
-            answers.append(self.sendCmdAndRead(cmd))
+            answers[cmd] = self.sendCmdAndRead(cmd)
 
         return answers
 
@@ -160,15 +188,20 @@ class KacoRS485(object):
         print("answers",answers)
 
         parsed = []
-        for a in answers:
-            if len(a) == 0:
+        for k in answers:
+            item = answers[k]
+            if len(item) == 0:
                 continue
-            if len(a) > 1:
-                raise Exception('answer of sent command was more than one line')
-            parsed.append(P.parse(a[0]))
+            parsed.append(P.parse(item,k))
+
+        #all answers could be empty, what should we do?
+        #we could also silently answer an empty dict
+        #but we prefer to raise an exception
+        if len(parsed) <= 0:
+            raise Exception('Could not get an answer from the inverter number {}; Answer: {:s}'.format(inverterNumber, repr(answers)))
 
         return P.listDictNameToKey(parsed)
-            
+
 
     def sendCmdAndRead(self,cmd):
         import time
@@ -176,12 +209,14 @@ class KacoRS485(object):
         send command on rs485 and read answer
 
         return list of answered lines
-        if no answer after waiting time, return empty list 
+        if no answer after waiting time, return empty list
         """
 
         #can only send bytearrays
         bytearr = cmd.encode()
         self.ser.write(bytearr)
+
+        print("send to rs485",bytearr)
 
         #wait some time to let device answer
         time.sleep(self.waitBeforeRead)
@@ -195,7 +230,4 @@ class KacoRS485(object):
         while self.ser.inWaiting() > 0:
             answer.append(self.ser.readline())
 
-        return answer
-
-
-
+        return ''.join(answer)
